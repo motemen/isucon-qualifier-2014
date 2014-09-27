bb43a5ca4fd4ce31988bee650cc69c32abca5463
y_uuki <yuki.tsubo@gmail.com>
2014-09-27 13:53:58 +0900

    ワーカをコア数ぐらいに

--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
 exec carton exec plackup                                 \
     -s Starlet                                           \
     --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
+    --max-workers=4 \
     --host localhost:8080 -E prod app.psgi
