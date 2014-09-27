6e2b939b930a2100129e5acbb93061396546e84e
motemen <motemen@gmail.com>
2014-09-27 11:36:35 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 set -x
-ssh isucon@54.168.155.12 'cd ~/deploy && git pull && supervisorctl restart isucon_perl'
+ssh isucon@54.168.155.12 '/home/isucon/notify.sh deploying...; cd ~/deploy && git pull && supervisorctl restart isucon_perl && /home/isucon/notify.sh deployed'
