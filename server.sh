8c971e82d5b9acb5926bfffb5809b1e263112413
Songmu <y.songmu@gmail.com>
2014-09-27 16:54:44 +0900

    app.sockの場所変更

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
-exec carton exec start_server --path /var/lib/nginx/app.sock \
-    -- plackup -s Starlet                                    \
-    --max-reqs-per-child=50000 --min-reqs-per-child=5000     \
+exec carton exec start_server --path /tmp/app.sock       \
+    -- plackup -s Starlet                                \
+    --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
     --max-workers=8 \
     -E prod app.psgi
