8bead4d01279e18f503f0b6ad4ac81dfbae6daf8
y_uuki <yuki.tsubo@gmail.com>
2014-09-27 14:04:59 +0900

    コメントひどい

--- server.sh
+++ server.sh
@@ -3,5 +3,5 @@ exec carton exec plackup                                 \
     --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
     --max-workers=4 \
     --socket-path=/var/lib/nginx/app.sock \
-    # --host localhost:8080 \
     -E prod app.psgi
+    # --host localhost:8080 \
