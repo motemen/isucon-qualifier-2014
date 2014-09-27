9e8dad177192ba5132aa38f395b692717245f6da
y_uuki <yuki.tsubo@gmail.com>
2014-09-27 14:10:22 +0900

    unix domain socket やめる

--- server.sh
+++ server.sh
@@ -2,6 +2,5 @@ exec carton exec plackup                                 \
     -s Starlet                                           \
     --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
     --max-workers=4 \
-    --socket-path=/var/lib/nginx/app.sock \
+    --host localhost:8080 \
     -E prod app.psgi
-    # --host localhost:8080 \
