78a1fe3ad3c7f4507a368dd2f4532585b3ff07d5
y_uuki <yuki.tsubo@gmail.com>
2014-09-27 14:14:23 +0900

    ワーカー数増やす

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 exec carton exec plackup                                 \
     -s Starlet                                           \
     --max-reqs-per-child=50000 --min-reqs-per-child=5000 \
-    --max-workers=4 \
+    --max-workers=8 \
     --host localhost:8080 \
     -E prod app.psgi
