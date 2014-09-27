d182b8b3364759ce36d08cd3faeee062f029f57c
motemen <motemen@gmail.com>
2014-09-27 12:53:30 +0900

    deploysh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 set -x
-ssh isucon@54.168.155.12 '/home/isucon/notify.sh deploying...; cd ~/deploy && git pull && supervisorctl restart isucon_perl && /home/isucon/notify.sh deployed'
+me=$(whoami)
+ssh isucon@54.168.155.12 "/home/isucon/notify.sh '$me deploying...'; cd ~/deploy && git pull && supervisorctl restart isucon_perl && /home/isucon/notify.sh deployed: \$(git log -1 --pretty=oneline)"
